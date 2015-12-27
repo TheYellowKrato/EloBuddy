1ebffcdf3ace6b2d63933c3aa8a87ae71b051e59
TheYellowKrato <maxime.desmarchelier@gmail.com>
2015-12-28 00:56:45 +0100

    Add LaneClear feature
    Use Q to make damage on 2 minions minimum

--- KarmaTo/KarmaTo/Modes/LaneClear.cs
+++ KarmaTo/KarmaTo/Modes/LaneClear.cs
@@ -1,4 +1,7 @@
-﻿using EloBuddy.SDK;
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using System.Linq;
+using Settings = KarmaTo.Config.Modes.LaneClear;
 //AA minions in order to have multiple minion to one shot with Q
 //Use Q to touch the most of minion killable 
 namespace KarmaTo.Modes
@@ -12,7 +15,35 @@ namespace KarmaTo.Modes
 
         public override void Execute()
         {
-            
+            AutoClear();
+        }
+        //Different logic : Use Q to make damage on minion
+        //TODO : Add 2 or 3 in settings
+        private void AutoClear()
+        {
+            var minions = Orbwalker.LaneclearMinions;
+            foreach (Obj_AI_Minion target in minions)
+            {
+                float x = Utils.getPlayer().Distance(target);
+                int nb = 1;
+                foreach (Obj_AI_Minion minion in minions)
+                {
+                    float y = target.Distance(minion);
+                    if (System.Math.Sqrt(x * x + y * y) >= Utils.getPlayer().Distance(minion))
+                    {
+                        nb++;
+                    }
+                }
+                if (nb > 1)
+                {
+                    var pred = Q.GetPrediction(target);
+                    if (!pred.Collision)
+                    {
+                        Q.Cast(target);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
